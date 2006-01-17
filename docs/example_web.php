cec32f79794f90fd689975e684bddf54df93c4ca
Nicholas Pisarro <taak@php.net>
2006-01-17 19:00:54 +0000

    - style issues - uses correct utf8 char length - added example of unicode block range detection
    git-svn-id: http://svn.php.net/repository/pear/packages/Text_LanguageDetect/trunk@205467 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/example_web.php
+++ docs/example_web.php
@@ -8,11 +8,14 @@
  */
 
 // browsers will encode multi-byte characters wrong unless they think the page is utf8-encoded
-header('Content-type: text/html; charset=utf-8',true);
+header('Content-type: text/html; charset=utf-8', true);
 
 require_once 'Text/LanguageDetect.php';
 
 $l = new Text_LanguageDetect;
+if (isset($_REQUEST['q'])) {
+    $q = stripslashes($_REQUEST['q']);
+}
 
 ?>
 <html>
@@ -22,12 +25,12 @@ $l = new Text_LanguageDetect;
 <body>
 <h2>Text_LanguageDetect</h2>
 <?
-echo "<small>supported languages:\n";
+echo "<small>Supported languages:\n";
 $langs = $l->getLanguages();
 sort($langs);
 foreach ($langs as $lang) {
-	echo ucfirst($lang), ', ';
-	$i++;
+    echo ucfirst($lang), ', ';
+    $i++;
 }
 
 echo "<br />total $i</small><br /><br />";
@@ -35,26 +38,35 @@ echo "<br />total $i</small><br /><br />";
 ?>
 <form method="post">
 Enter text to identify language (at least a couple of sentences):<br />
-<textarea name="q" wrap="virtual" cols="80" rows="8"><?= stripslashes($_REQUEST['q']) ?></textarea>
+<textarea name="q" wrap="virtual" cols="80" rows="8"><?= $q ?></textarea>
 <br />
 <input type="submit" value="Submit" />
 </form>
 <?
-if (isset($_REQUEST['q']) && $len = strlen($_REQUEST['q'])) {
-	if ($len < 50) { # this value was picked somewhat arbitrarily
-		echo "warning: string not very long ($len chars)<br />\n";
-	}
+if (isset($q) && strlen($q)) {
+    $len = $l->utf8strlen($q);
+    if ($len < 20) { // this value picked somewhat arbitrarily
+        echo "Warning: string not very long ($len chars)<br />\n";
+    }
 
-	$result = $l->detectConfidence($_REQUEST['q']);
+    $result = $l->detectConfidence($q);
 
-	if ($result == null) {
-		echo "Text_LanguageDetect cannot identify this piece of text.\n";
-	} else {
-		echo "Text_LanguageDetect thinks this text is written in <b>{$result['language']}</b> ({$result['similarity']}, {$result['confidence']})\n";
-	}
+    if ($result == null) {
+        echo "Text_LanguageDetect cannot identify this piece of text. <br /><br />\n";
+    } else {
+        echo "Text_LanguageDetect thinks this text is written in <b>{$result['language']}</b> ({$result['similarity']}, {$result['confidence']})<br /><br />\n";
+    }
+
+    $result = $l->detectUnicodeBlocks($q, false);
+    if (!empty($result)) {
+        arsort($result);
+        echo "Unicode blocks present: ", join(',', array_keys($result)), "\n<br /><br />";
+    }
 }
 
 unset($l);
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 ?>
 </body></html>
